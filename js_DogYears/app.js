8c6537b605cb2d7bb03aaf5917480e1de05d446f
aspahija <ante.spahija@gmail.com>
2018-07-28 17:52:57 +0200

    js_intro2

---
+++ js_DogYears/app.js
@@ -0,0 +1,18 @@
+const myAge = 32;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+// number of dog years accounted for by my later years
+laterYears *= 4;
+
+// sum of dog years accounted for my age
+let myAgeInDogYears = earlyYears + laterYears;
+
+// my name in lowercase letters
+let myName = 'Jimmy Bimmy'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`)
