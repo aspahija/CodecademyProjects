da593b039ed1d9463cdb052ac08e0ed863cc7172
aspahija <ante.spahija@gmail.com>
2018-08-16 23:58:20 +0200

    py_controlflow

---
+++ py_AreaCalculator/AreaCalculator.py
@@ -0,0 +1,22 @@
+"""
+Codecademy - Learn Python
+Area Calculator
+"""
+
+print "Starting the calculator..."
+
+option = raw_input("Enter C for Circle or T for Triangle: ")
+
+if option == 'C':
+  radius = float(raw_input("Enter radius: "))
+  area = 3.14159 * radius**2
+  print "Area: %f" % area
+elif option == 'T':
+  base = float(raw_input("Enter base: "))
+  height = float(raw_input("Enter height: "))
+  area = 0.5 * base * height
+  print "Area: %f" % area
+else:
+  print "Error! Invalid shape."
+
+print "Exiting..."
