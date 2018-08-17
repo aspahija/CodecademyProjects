42e1338970d05a633fcf51f8b1668e8ecc0eb78e
aspahija <ante.spahija@gmail.com>
2018-08-17 12:58:30 +0200

    py_classes

---
+++ py_BankAccount/bankaccount.py
@@ -0,0 +1,34 @@
+"""
+Bank account with classes
+"""
+
+class BankAccount(object):
+  balance = 0
+  def __init__(self, name):
+        self.name = name
+  def __repr__(self):
+        return "%s's account. Balance: $%.2f" % (self.name, self.balance)
+  def show_balance(self):
+        print ("%s's account. Balance:$%.2f" % (self.name, self.balance))
+  def   deposit(self, amount):
+    if amount <=0:
+        print ("You have no money")
+        return
+    else:
+        print ("Amount deposited: $%.2f" % amount)
+    self.balance += amount
+    self.show_balance()
+  def withdraw(self, amount):
+    if amount > self.balance:
+        print (" You can't do that")
+        return
+    else:
+        print ("Withdrawing: %.2f" % (amount))
+    self.balance -= amount
+    self.show_balance()
+    
+my_account = BankAccount("Jimmy")
+my_account.show_balance()
+my_account.deposit(2000)
+my_account.withdraw(1000)
+my_account.show_balance()
